65be5b41d05f8b506406f7b741d533b2ab4d14a8
legengliu <legengliu@berkeley.edu>
2017-09-15 01:12:02 -0700

    finish hw1

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -2,14 +2,16 @@ pragma solidity ^0.4.13;
 
 
 contract greeter {
-	/* Add one variable to hold our greeting */
-	string greeting;
+    /* Add one variable to hold our greeting */
+    string greeting;
 
-	function greeter(string _greeting) public {
-		/* Write one line of code for the contract to set our greeting */
-	}
+    function greeter(string _greeting) public {
+        /* Write one line of code for the contract to set our greeting */
+        greeting = _greeting;
+    }
 
-	function greet() constant returns (string)  {
-		/* Write one line of code to allow the contract to return our greeting */
-	}
+    function greet() constant returns (string)  {
+        /* Write one line of code to allow the contract to return our greeting */
+        return greeting;
+    }
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		if (position == 0) {
+			result = 0;
+		}
+		uint a = 0;
+		uint b = 1;
+		for (uint i = 2; i < position; i++) {
+			uint c = a + b;
+			a = b;
+			b = c;
+		}
+		result = a + b;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() { x = 1; }
+	uint x;
 }
